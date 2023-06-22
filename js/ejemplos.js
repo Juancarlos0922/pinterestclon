f60b6a1d29a4c2bebd2ac15ebfb3ccba9fa19390
Juan Carlos Zapata Gracia <juancarloszapata@gmail.com>
2023-06-22 15:21:37 -0500

    Add files via upload

---
+++ js/ejemplos.js
@@ -0,0 +1,35 @@
+// tipos de datos en variables
+
+
+let cadena = "juancarlos"
+let numeross = 1;
+let boleaan = true;
+let boolean = false;
+
+// Objeto en javascript
+
+
+let  tigre = {
+    color = amarillo,
+    Patas : = 4,
+    Muerde : = true,
+    Nombre : = "pegasus"
+}
+
+// Arreglo o array
+
+
+let arreglo = [8,6,5,4];
+
+let frutas = ["limon", "Manzanas", "peras"];
+
+// Operadores
+
+let a = 2;
+let b = 4;
+
+// Operadores relacionales
+
+// realiza comparación y devuelve resultado true o false
+
+5 == 5
---
+++ js/funtion.js
@@ -0,0 +1,8 @@
+
+let a = 5;
+let b = 5;
+let c = 7;
+let d = 8;
+
+let res =  a == b
+
